619d6a6303a3df1b9333acd037891207af8591f0
ignacioIA <ignaciocruz1@hotmail.com>
2026-08-14 20:53:33 -0300

    Primer commit

---
+++ src/main/java/com/IntraNet/Laucom/LaucomApplication.java
@@ -0,0 +1,13 @@
+package com.IntraNet.Laucom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LaucomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LaucomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/IntraNet/Laucom/LaucomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.IntraNet.Laucom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LaucomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
